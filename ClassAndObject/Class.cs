2b8fdb3eb63c14f202f25c17cbff2916606037f4
pfdah <pratikdahal700@gmail.com>
2020-11-18 13:41:18 +0545

    Lab 2 qn 1

---
+++ ClassAndObject/Class.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndObject
+{
+    class Class
+    {
+        public string message = "This is a class item. It can be accessed from an object";
+        private string secret_message = "This is secret message and can be accessed through method only";
+        public void print_secret()
+        {
+            Console.WriteLine(secret_message);
+        }
+    }
+}
---
+++ ClassAndObject/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassAndObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class obj = new Class();
+            Console.WriteLine(obj.message);
+            obj.print_secret();
+
+        }
+    }
+}
